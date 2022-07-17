4e1ca032c64a687f742d7f1b4fed71b5a8decd1f
yoshifumitsuji <yoshifumi.tsuji@gmail.com>
2022-07-17 12:09:43 +0900

    fix typo

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   typescript: { reactDocgen: false },
-  stories: ['../src/**/*.stories.mdx', '../src/cmoponents'],
+  stories: ['../src/**/*.stories.mdx', '../src/components'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   staticDirs: ['../public'],
