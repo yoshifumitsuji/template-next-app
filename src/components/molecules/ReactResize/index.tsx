c969bc41c735883fce4f04e030e86719dacae44a
yoshifumitsuji <yoshifumi.tsuji@gmail.com>
2022-10-16 21:19:03 +0900

    display width and height in react resize component

--- src/components/molecules/ReactResize/index.tsx
+++ src/components/molecules/ReactResize/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useResizeObserver } from 'src/hooks/useResizeObserver'
 
@@ -7,6 +7,10 @@ import styles from './index.module.scss'
 type ReactResizeProps = {}
 
 export const ReactResize = (props: ReactResizeProps) => {
-  const { ref } = useResizeObserver<HTMLDivElement>(() => console.log('resize!'))
-  return <div ref={ref} className={styles.box}></div>
+  const [size, setSize] = useState({ width: 0, height: 0 })
+  const { ref: block } = useResizeObserver<HTMLDivElement>((entries: any) => {
+    const { width, height } = entries[0].contentRect
+    setSize({ width, height })
+  })
+  return <div ref={block} className={styles.block}>{`width: ${size.width} / height: ${size.height}`}</div>
 }
